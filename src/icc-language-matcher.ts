c8ee66ee31da13030e7a37568f89f20ec565c468
Florian Völker <Neirolf2030@web.de>
2019-12-30 12:54:57 +0100

    removed module wrapper

--- src/icc-language-matcher.ts
+++ src/icc-language-matcher.ts
@@ -1,17 +1,14 @@
 import { positiveLookahead } from './reg-exp-helper';
 
-export default (() => {
+const lineBreak = '(?:\\r?\\n)'
 
-    const lineBreak = '(?:\\r?\\n)'
+const frontOrBackKeywords = lineBreak + '{2}(Front|Back):' + lineBreak + '{2}';
+const cardContentMatch = '((?:' + lineBreak + '|.)+?)';
 
-    const frontOrBackKeywords = lineBreak + '{2}(Front|Back):' + lineBreak + '{2}';
-    const cardContentMatch = '((?:' + lineBreak + '|.)+?)';
+const metaNameMatcher = '(\\w+)';
+const metaFieldMatcher = '(.+)';
 
-    const metaNameMatcher = '(\\w+)';
-    const metaFieldMatcher = '(.+)';
+const REGEXP_ICC_META_MATCHER = '(?:' + lineBreak + '|^)' + 'ICC-' + metaNameMatcher + ': ?' + metaFieldMatcher + positiveLookahead(lineBreak);
+const REGEXP_ICC_CARD_SIDE_MATCHER = frontOrBackKeywords + cardContentMatch + positiveLookahead('(:?' + frontOrBackKeywords + ')|$');
 
-    const REGEXP_ICC_META_MATCHER = '(?:' + lineBreak + '|^)' + 'ICC-' + metaNameMatcher + ': ?' + metaFieldMatcher + positiveLookahead(lineBreak);
-    const REGEXP_ICC_CARD_SIDE_MATCHER = frontOrBackKeywords + cardContentMatch + positiveLookahead('(:?' + frontOrBackKeywords + ')|$');
-
-    return Object.freeze({ REGEXP_ICC_META_MATCHER, REGEXP_ICC_CARD_SIDE_MATCHER });
-})();
+export default Object.freeze({ REGEXP_ICC_META_MATCHER, REGEXP_ICC_CARD_SIDE_MATCHER });
--- src/icc-parser.ts
+++ src/icc-parser.ts
@@ -2,73 +2,71 @@ import ICCLanguageMatcher from './icc-language-matcher';
 import { ICCDocument, ICCCard, ICCMeta } from './icc-types';
 import { getAll } from './reg-exp-helper';
 
-export default (() => {
-    class ICCParserError extends Error {
-        constructor(msg: string) { super(msg); }
-    }
+class ICCParserError extends Error {
+    constructor(msg: string) { super(msg); }
+}
 
-    const LineBreakMatcher = new RegExp('\r\n', 'g');
-    const toNewLineOnly = (str: string) => (LineBreakMatcher.lastIndex = 0, str.replace(LineBreakMatcher, '\n'));
+const LineBreakMatcher = new RegExp('\r\n', 'g');
+const toNewLineOnly = (str: string) => (LineBreakMatcher.lastIndex = 0, str.replace(LineBreakMatcher, '\n'));
 
-    const REGEXP_ICC_META = new RegExp(ICCLanguageMatcher.REGEXP_ICC_META_MATCHER, 'g');
-    const REGEXP_ICC_CARD_SIDE = new RegExp(ICCLanguageMatcher.REGEXP_ICC_CARD_SIDE_MATCHER, 'g');
+const REGEXP_ICC_META = new RegExp(ICCLanguageMatcher.REGEXP_ICC_META_MATCHER, 'g');
+const REGEXP_ICC_CARD_SIDE = new RegExp(ICCLanguageMatcher.REGEXP_ICC_CARD_SIDE_MATCHER, 'g');
 
-    return function parse(str: string) {
-        REGEXP_ICC_META.lastIndex = 0;
-        REGEXP_ICC_CARD_SIDE.lastIndex = 0;
+export default function parse(str: string) {
+    REGEXP_ICC_META.lastIndex = 0;
+    REGEXP_ICC_CARD_SIDE.lastIndex = 0;
 
-        const metaMatchResult = (() => { try { return getAll(REGEXP_ICC_META, str) } catch (e) { throw new ICCParserError("could not parse the meta part of the document") } })();
+    const metaMatchResult = (() => { try { return getAll(REGEXP_ICC_META, str) } catch (e) { throw new ICCParserError("could not parse the meta part of the document") } })();
 
-        if (metaMatchResult.matches.length == 0 || metaMatchResult.matches[0][1] != 'Version')
-            throw new ICCParserError('the first field present in the document needs to be ICC-Version');
+    if (metaMatchResult.matches.length == 0 || metaMatchResult.matches[0][1] != 'Version')
+        throw new ICCParserError('the first field present in the document needs to be ICC-Version');
 
-        if (metaMatchResult.matches[0][2] != '1')
-            throw new ICCParserError('can not parse other ICC-Versions than 1');
+    if (metaMatchResult.matches[0][2] != '1')
+        throw new ICCParserError('can not parse other ICC-Versions than 1');
 
-        REGEXP_ICC_CARD_SIDE.lastIndex = metaMatchResult.lastIndex;
-        const cardsMatchResult = (() => { try { return getAll(REGEXP_ICC_CARD_SIDE, str) } catch (e) { throw new ICCParserError("could not parse the cards part of the document") } })();
+    REGEXP_ICC_CARD_SIDE.lastIndex = metaMatchResult.lastIndex;
+    const cardsMatchResult = (() => { try { return getAll(REGEXP_ICC_CARD_SIDE, str) } catch (e) { throw new ICCParserError("could not parse the cards part of the document") } })();
 
-        const metaObjects: ICCMeta[] = [];
-        for (const [, field, value] of metaMatchResult.matches)
-            metaObjects.push({ field, value })
+    const metaObjects: ICCMeta[] = [];
+    for (const [, field, value] of metaMatchResult.matches)
+        metaObjects.push({ field, value })
 
-        const cardsObjects: ICCCard[] = [];
+    const cardsObjects: ICCCard[] = [];
 
-        let bufferSide = true;
-        let buffer: undefined | string;
+    let bufferSide = true;
+    let buffer: undefined | string;
 
-        for (const [, sideIdentifier, cardContent] of cardsMatchResult.matches) {
-            const side = sideIdentifier == 'Front';
-            const formattedCC = side ? cardContent : toNewLineOnly(cardContent);
-
-            if (buffer) {
-                if (bufferSide == side) {
-                    cardsObjects.push({
-                        [bufferSide ? 'front' : 'back']: buffer
-                    });
-
-                    bufferSide = side;
-                    buffer = formattedCC;
-                } else {
-                    cardsObjects.push({
-                        [bufferSide ? 'front' : 'back']: buffer,
-                        [side ? 'front' : 'back']: formattedCC,
-                    });
-
-                    buffer = undefined;
-                }
-            } else {
-                bufferSide = side;
-                buffer = formattedCC;
-            }
-        }
+    for (const [, sideIdentifier, cardContent] of cardsMatchResult.matches) {
+        const side = sideIdentifier == 'Front';
+        const formattedCC = side ? cardContent : toNewLineOnly(cardContent);
 
         if (buffer) {
-            cardsObjects.push({
-                [bufferSide ? 'front' : 'back']: buffer
-            });
-        }
+            if (bufferSide == side) {
+                cardsObjects.push({
+                    [bufferSide ? 'front' : 'back']: buffer
+                });
 
-        return { metaInformation: metaObjects, cards: cardsObjects } as ICCDocument;
+                bufferSide = side;
+                buffer = formattedCC;
+            } else {
+                cardsObjects.push({
+                    [bufferSide ? 'front' : 'back']: buffer,
+                    [side ? 'front' : 'back']: formattedCC,
+                });
+
+                buffer = undefined;
+            }
+        } else {
+            bufferSide = side;
+            buffer = formattedCC;
+        }
     }
-})();
+
+    if (buffer) {
+        cardsObjects.push({
+            [bufferSide ? 'front' : 'back']: buffer
+        });
+    }
+
+    return { metaInformation: metaObjects, cards: cardsObjects } as ICCDocument;
+}
